6970631aa8191203d3bae81a545f441353b2285d
Scott Lobdell <slobdell@hearsaycorp.com>
2014-07-22 20:52:20 -0700

    django template conversion start

--- airborne_imagery/basic_navigation/views.py
+++ airborne_imagery/basic_navigation/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render_to_response
 
 def home(request):
     render_data = {}
-    return render_to_response("template.html", render_data)
+    return render_to_response("index.html", render_data)
