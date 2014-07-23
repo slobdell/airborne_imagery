c4769ef2bf1de8bc8b108a2d67dde48b4a854b89
Scott Lobdell <slobdell@hearsaycorp.com>
2014-07-23 13:54:00 -0700

    converting more templates to django

--- airborne_imagery/basic_navigation/views.py
+++ airborne_imagery/basic_navigation/views.py
@@ -3,4 +3,32 @@ from django.shortcuts import render_to_response
 
 def home(request):
     render_data = {}
-    return render_to_response("index.html", render_data)
+    return render_to_response("basic_navigation/index.html", render_data)
+
+
+def two_columns(request):
+    return render_to_response("basic_navigation/page_2_columns_left.html", {})
+
+
+def about(request):
+    return render_to_response("basic_navigation/page_about_me.html", {})
+
+
+def contact(request):
+    return render_to_response("basic_navigation/page_contact2.html", {})
+
+
+def invoice(request):
+    return render_to_response("basic_navigation/page_invoice.html", {})
+
+
+def privacy(request):
+    return render_to_response("basic_navigation/page_privacy.html", {})
+
+
+def registration(request):
+    return render_to_response("basic_navigation/page_registration.html", {})
+
+
+def portfolio(request):
+    return render_to_response("basic_navigation/portfolio_text_blocks.html", {})
--- airborne_imagery/urls.py
+++ airborne_imagery/urls.py
@@ -1,10 +1,21 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
 
 from .basic_navigation.views import home
+from .basic_navigation.views import two_columns
+from .basic_navigation.views import about
+from .basic_navigation.views import contact
+from .basic_navigation.views import invoice
+from .basic_navigation.views import privacy
+from .basic_navigation.views import registration
+from .basic_navigation.views import portfolio
 
 urlpatterns = patterns('',
     url(r'^$', home, name='home'),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^two_columns/$', two_columns),
+    url(r'^about/$', about),
+    url(r'^contact/$', contact),
+    url(r'^invoice/$', invoice),
+    url(r'^privacy/$', privacy),
+    url(r'^registration/$', registration),
+    url(r'^portfolio/$', portfolio),
 )
