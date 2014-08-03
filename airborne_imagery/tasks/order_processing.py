d6ac0c4a0b842427a7ce5f7362ab9da47d1d0109
Scott Lobdell <slobdell@hearsaycorp.com>
2014-08-02 18:45:37 -0700

    Add logging

--- airborne_imagery/tasks/order_processing.py
+++ airborne_imagery/tasks/order_processing.py
@@ -29,12 +29,17 @@ def resize_images_for_order(order):
         downloader = BotoDownloader(picture.amazon_key)
         new_filename = "%s/%s_%s.jpg" % (save_dir, picture.id, intended_dimensions)
         downloader.download(new_filename)
+        print "Downloading original image from Amazon to %s" % new_filename
         pil_img = Image.open(new_filename)
         width, height = pil_img.size
         if height > width:
+            print "Rotating image to landscape"
             pil_img = pil_img.rotate(90)
+        print "Resizing image with new width: %s" % new_width
         pil_img = resize(pil_img, new_width)
         pil_img.save(new_filename)
         public_filename = "completed_orders/%s.jpg" % encode(new_filename)
+        print "Uploading image to amazon with new filename: %s" % public_filename
         final_amazon_path = "%s/%s" % (BASE_URL, BotoUploader.upload_single_file(new_filename, public_filename))
         order.update_picture_id_with_finish_url(picture.id, final_amazon_path)
+        print "Finished updating Picture %s with final URL %s" % (picture.id, final_amazon_path)
