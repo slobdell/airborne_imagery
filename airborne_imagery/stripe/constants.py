f3a93c68201242d0f89025c75b2175ab152ea4a0
Scott Lobdell <slobdell@hearsaycorp.com>
2014-10-23 15:38:34 -0700

    remove stripe key

--- airborne_imagery/stripe/constants.py
+++ airborne_imagery/stripe/constants.py
@@ -1,8 +1,3 @@
-TEST_SECRET_KEY = 'sk_test_4VYAw8I9GLthRU6xpun79EfQ'
-TEST_PUBLISHABLE_KEY = 'pk_test_4VYAlh6AKX6wLlnMb9HpTKzG'
-
-LIVE_SECRET_KEY = 'sk_live_4VYAxQ8vfBi6bEhrtRVzB1oZ'
-LIVE_PUBLISHABLE_KEY = 'pk_live_4VYAMDUSv8kCEevI88iQAylH'
 
 
 def get_secret_key():
